efaba3bb44dbf288f811833592999fe7d7017790
benjaminesser <bencesser@gmail.com>
2023-01-18 12:50:54 -0800

    Create SearchEngine.java

---
+++ SearchEngine.java
@@ -0,0 +1,60 @@
+import java.io.IOException;
+import java.net.URI;
+import java.util.ArrayList;
+
+class Handler implements URLHandler {
+    // The one bit of state on the server: a number that will be manipulated by
+    // various requests.
+    int num = 0;
+    ArrayList<String> sList = new ArrayList<String>();
+
+    public String handleRequest(URI url) {
+        if (url.getPath().equals("/")) {
+            return String.format("Ben's number: %d", num);
+        } else if (url.getPath().equals("/increment")) {
+            num += 1;
+            return String.format("Number incremented!");
+        } else {
+            System.out.println("Path: " + url.getPath());
+            if (url.getPath().contains("/add")) {
+                String[] parameters = url.getQuery().split("=");
+                if (parameters[0].equals("count")) {
+                    num += Integer.parseInt(parameters[1]);
+                    return String.format("Number increased by %s! It's now %d", parameters[1], num);
+                }
+                if (parameters[0].equals("s")) {
+                    sList.add(parameters[1]);
+                    return String.format("%s added!", parameters[1]);
+                }
+            }
+            if (url.getPath().contains("/search")) {
+                String[] parameters = url.getQuery().split("=");
+                if (parameters[0].equals("s")) {
+                    String returnString = "";
+                    for (String s: sList) {
+                        if (s.contains(parameters[1])) {
+                            returnString = returnString + s + "\n";
+                        }
+                    }
+                    return String.format("These strings contains those characters: %s", returnString);
+                }
+            }
+
+            return "404 Not Found!";
+        }
+    }
+}
+
+class SearchEngine {
+    public static void main(String[] args) throws IOException {
+        if(args.length == 0){
+            System.out.println("Missing port number! Try any number between 1024 to 49151");
+            return;
+        }
+
+        int port = Integer.parseInt(args[0]);
+
+        Server.start(port, new Handler());
+    }
+}
+
